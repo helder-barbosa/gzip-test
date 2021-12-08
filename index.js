402c3fb38f205a067f8b430a7ed554ba673589df
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-12-07 22:25:30 -0300

    #1 GZIP - Compactando a transferência de dados

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const compression = require('compression')
+
+const largeObj = []
+
+for (let i = 0; i < 50000; i++) {
+  largeObj.push({
+    name: "Person " + i,
+    address: "address " + i,
+    description: "description"
+  })
+}
+
+app.use(compression())
+app.get('/', (req, res) => res.send(largeObj))
+
+app.listen(3000, () => console.log('[SERVER] Running...'))
